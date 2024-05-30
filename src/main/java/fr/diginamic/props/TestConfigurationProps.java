266de3c7b22b2bc2364e0d1c99052192712e73dc
ThomasdevLC <thomas.lecam@gmail.com>
2024-05-30 13:54:42 +0200

    conection database

--- src/main/java/fr/diginamic/props/TestConfigurationProps.java
+++ src/main/java/fr/diginamic/props/TestConfigurationProps.java
@@ -1,23 +1,37 @@
 package fr.diginamic.props;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.ResourceBundle;
 
 public class TestConfigurationProps {
 
-	public static void main(String[] args) {
-		ResourceBundle config  = ResourceBundle.getBundle("config");
-		String url = config.getString("database.url");
-		
-		Iterator<String> iterateur = config.getKeys().asIterator();
-		while (iterateur.hasNext())	{
-			String key = iterateur.next();
-			System.out.println(key);
-			
-		}
-		System.out.println(url);
+    public static void main(String[] args) {
+        ResourceBundle config = ResourceBundle.getBundle("config");
+        String url = config.getString("database.url");
+        String user = config.getString("database.user");
+        String pwd = config.getString("database.password");
 
+        Iterator<String> iterateur = config.getKeys().asIterator();
+        while (iterateur.hasNext()) {
+            String key = iterateur.next();
+            System.out.println(key);
+        }
+        System.out.println(url);
 
-	}
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            Connection conn = DriverManager.getConnection(url, user, pwd);
 
+            System.out.println(conn.isClosed());
+
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
+
